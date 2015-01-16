bc662ea9213ddc8dc673b89cb2497ea6942f792c
root <root@ubuntu.(none)>
2015-01-16 16:41:45 -0500

    New version 0.2

--- build.sh
+++ build.sh
@@ -1,1 +1,15 @@
-dpkg-deb --build elpack_0.1-2
+#!/bin/bash
+vo=`cat version`
+vn=$((vo+1))
+git mv elpack_0.$vo elpack_0.$vn
+sed -e "s/^Version.*/Version: 0.$vn/" -i elpack_0.${vn}/DEBIAN/control
+
+dpkg-deb --build elpack_0.$vn deb
+
+echo $vn > version
+
+git add *
+
+git commit -m "New version 0.${vn}"
+
+#git push
